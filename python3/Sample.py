0fe0c0b20cfc50cbcd5fa3110f1dd1f07c891c6d
choogoo <tyutluyc@gmail.com>
2022-05-06 17:49:16 +0000

    --

--- python3/Sample.py
+++ python3/Sample.py
@@ -16,16 +16,17 @@ if __name__ == "__main__":
    1.第三方回复加密消息给公众平台；
    2.第三方收到公众平台发送的消息，验证消息的安全性，并对消息进行解密。
    """
-   encodingAESKey = "2AtfpPTPyWMj**************V3oJYX8y0nhd" 
-   token = "uePZd*****************FFadSrIS"
+   encodingAESKey = "2AtfpT4BS**********hd" 
+   token = "uePZdAFJv********IS"
    nonce = "1320562132"
-   appid = "tR36********yG6Y"
+   appid = "tR36***Ms4TyG6Y"
+   USERID = 'fbkrb75TNaT'
 
-   to_xml = """<xml><managerid><![CDATA[fbk*****NaT]]></managerid><skill><skillname><![CDATA[技能名称1]]></skillname><title><![CDATA[标准问题2]]></title><question><![CDATA[相似问题1]]></question><question><![CDATA[相似问题2]]></question><question><![CDATA[相似问题3]]></question><answer><![CDATA[1]]></answer><answer><![CDATA[2]]></answer></skill><skill><skillname><![CDATA[技能名称1]]></skillname><title><![CDATA[标准问题3]]></title><question><![CDATA[相似问题1]]></question><answer><![CDATA[1]]></answer></skill></xml>"""
+   to_xml = """<xml><managerid><![CDATA[fbkrb75TNaT]]></managerid></xml>"""
 
    #测试加密接口
    encryp_test = WXBizMsgCrypt(token,encodingAESKey,appid)
-   ret,encrypt_xml = encryp_test.EncryptMsg(to_xml,nonce)   
+   ret,encrypt_xml = encryp_test.EncryptMsg(to_xml,nonce,'1651858977574')   
    print(ret,encrypt_xml)
 
    dict_xml = xmltodict.parse(encrypt_xml)
@@ -34,8 +35,10 @@ if __name__ == "__main__":
    print(jsonStr)
    jsonObj = json.loads(jsonStr)
    encrypt = jsonObj['xml']['Encrypt']
+   print(to_xml)
+   print(encrypt)
 
-   url = 'https://openai.weixin.qq.com/openapi/batchimportskill/'+token
+   url = 'https://openai.weixin.qq.com/openapi/publish/'+token
    data = {"encrypt": encrypt}
    res = requests.post(url=url,json=data)
    print(res.text)
